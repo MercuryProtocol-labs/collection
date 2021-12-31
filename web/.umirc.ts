7b9d69a1809eaae49092f8f2c42deff52d22697e
bolingboling <zhangxiaoshang66@163.com>
2021-12-31 17:01:17 +0800

    update publicpath

--- web/.umirc.ts
+++ web/.umirc.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  publicPath: '/collection/',
+  // publicPath: '/collection/',
   hash: true,
   history: { type: 'hash' },
   theme: {
